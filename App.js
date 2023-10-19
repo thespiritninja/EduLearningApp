979a457bad072c348b7caf34b253fafe8dcafadc
Shreyas Sawant <shreyassawant018@gmail.com>
2023-10-18 21:27:55 -0400

    fixed login with google

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import OnboardingScreen from './components/Onboarding';
 import Login from './components/Login';
-import Constants from 'expo-constants';
 export default function App() {
   const [moveToLogin, setMoveToLogin] = useState(null);
-
   return (
     <View style={styles.container}>
       {!moveToLogin ? 
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,23 +1,53 @@
-import { Text, StyleSheet, View, Image } from 'react-native'
-import React, { Component } from 'react'
+import { Text, StyleSheet, View, Image, TouchableOpacity } from 'react-native'
+import React, { Component, useEffect, useState } from 'react'
 import {Ionicons} from '@expo/vector-icons'
 import Color from '../assets/utils/Color'
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import {CLIENT_ID, IOS_ID} from '@env';
+
 
 export default Login = () => {
+    WebBrowser.maybeCompleteAuthSession();
+    const [accessToken, setAccessToken] = useState();
+    const [userInfo, setUserInfo] = useState({});
+    const [request, response, promptAsync] = Google.useAuthRequest({
+       clientId: CLIENT_ID, 
+       iosClientId: IOS_ID
+    })
+    useEffect(()=>{
+        if(response?.type === "success"){
+            setAccessToken(response.authentication.accessToken);
+            console.log(accessToken);
+            getUserInfo();
+        }
+    },[response])
+
+    const getUserInfo = async () => {
+        try{
+            const response = await fetch("https://www.googleapis.com/userinfo/v2/me",{headers: {Authorization: `Bearer ${accessToken}`}});
+            const user = await response.json();
+            setUserInfo(user);
+            console.log(user);
+        }
+        catch(err){
+            console.log(err);
+        }
+    };
         return (
-            <View style={{alignContent: 'center'}}>
+            <View style={{alignContent: 'center', justifyContent: 'space-evenly', marginTop: 'auto'}}>
                 <Image source={require('../assets/images/login-illustration.png')} style={styles.imageContainer} />
                 <View style={styles.container}>
                     <Text style={styles.greetingTexts}>YouGots2Code!</Text>
                     <Text style={styles.subText}>Login/Signup</Text>
-                    <View style={styles.button}>
+                    <TouchableOpacity style={styles.button} onPress={() => {promptAsync()}}>
                         <Ionicons name='logo-google' size={25} color='black' style={{marginRight: 5}}/>
                             <Text style={{textAlign:'center', marginTop: 5}}>Login with Google!</Text>
-                    </View>
-                    <View style={styles.button}>
+                    </TouchableOpacity>
+                    {/* <TouchableOpacity style={styles.button} onPress={()=>{}}>
                         <Ionicons name='logo-linkedin' size={25} color='black' style={{marginRight: 5}}/>
                             <Text style={{textAlign:'center', marginTop: 5}}>Login with LinkedIn!</Text>
-                    </View>
+                    </TouchableOpacity> */}
                 </View>
             </View>
         )
